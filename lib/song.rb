fc18d2df2317514131a6650b4f82e538a2055971
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-12-08 19:36:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,5 @@
 class Song
-  attr_accessor :name :artist :genre
+  attr_accessor :name, :artist, :genre
 
   @@count = 0
   @@genre = []
@@ -30,7 +30,7 @@ class Song
   end
 
   def self.genre_count
-    
+
   end
 
 end
